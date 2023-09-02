cb909c8d28f10bc10e8928a778a3e36fac179e65
aakashshankar <aakashshankar217@gmail.com>
2023-09-02 17:42:34 -0400

    Make all transcriptions session aware

--- core/src/main/java/com/speechtotext/core/controller/SpeechToTextController.java
+++ core/src/main/java/com/speechtotext/core/controller/SpeechToTextController.java
@@ -9,16 +9,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import reactor.core.publisher.Flux;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.Duration;
-import java.util.UUID;
 
 import static java.lang.String.format;
 
@@ -37,35 +36,27 @@ public class SpeechToTextController {
     @Inject
     private SpeechCategorizer speechCategorizer;
 
-    @PostMapping(value = "/stream/transcribe", consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
-            produces = MediaType.TEXT_PLAIN_VALUE)
-    @CrossOrigin(origins = "*")
-    public Flux<String> getText(@RequestPart("audio") MultipartFile audioFile) throws IOException {
-        byte[] content = convertToWav(audioFile, UUID.randomUUID().toString());
-        String[] transcript = speechToTextRecognizer.convert(content).split(" ");
-        return Flux.interval(Duration.ofMillis(100)).take(transcript.length)
-                .map(f -> format("%s ", transcript[f.intValue()]));
-    }
-
-    @PostMapping(value = "/categorize", consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+    @PostMapping(value = "categorize", consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     @CrossOrigin(origins = "*")
-    public CategorizedSpeechResponse categorize(@RequestPart("audio") MultipartFile audioFile) throws IOException {
-        String userId = UUID.randomUUID().toString();
-        byte[] content = convertToWav(audioFile, userId);
+    public ResponseEntity<CategorizedSpeechResponse> categorize(@RequestHeader("x-session-id") String sessionId,
+                                                                @RequestPart("audio") MultipartFile audioFile) throws IOException {
+        byte[] content = convertToWav(audioFile, sessionId);
         String transcript = speechToTextRecognizer.convert(content);
         LOGGER.info("Transcribed audio to: {}", transcript);
 
         CategorizedSpeechRequest request = new CategorizedSpeechRequest();
         request.setTranscription(transcript);
-        request.setUserId(userId);
-        return speechCategorizer.getCategoriesFromApi(request);
+        request.setSessionId(sessionId);
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(speechCategorizer.getCategoriesFromApi(request));
     }
 
     private byte[] convertToWav(MultipartFile audioFile, String userId) throws IOException {
-        File webmFile = Files.write(Paths.get(format("%s\\input-%s.webm", intermediaryAudioStoragePath, userId)),
+        File webmFile = Files.write(Paths.get(format("%s/input-%s.webm", intermediaryAudioStoragePath, userId)),
                 audioFile.getBytes()).toFile();
-        String resultLocation = format("%s\\final-%s.wav", intermediaryAudioStoragePath, userId);
+        String resultLocation = format("%s/final-%s.wav", intermediaryAudioStoragePath, userId);
 
         String command = format("ffmpeg -i %s %s", webmFile.getAbsolutePath(), resultLocation);
 
--- core/src/main/java/com/speechtotext/core/service/SpeechToTextRecognizer.java
+++ core/src/main/java/com/speechtotext/core/service/SpeechToTextRecognizer.java
@@ -1,6 +1,10 @@
 package com.speechtotext.core.service;
 
-import com.google.cloud.speech.v1.*;
+import com.google.cloud.speech.v1.RecognitionAudio;
+import com.google.cloud.speech.v1.RecognitionConfig;
+import com.google.cloud.speech.v1.RecognizeResponse;
+import com.google.cloud.speech.v1.SpeechClient;
+import com.google.cloud.speech.v1.SpeechRecognitionResult;
 import com.google.protobuf.ByteString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +20,6 @@ public class SpeechToTextRecognizer {
 
     public String convert(byte[] audioContent) {
         try (SpeechClient speechClient = SpeechClient.create()) {
-            LOGGER.info("Created speechClient: {} for audio content: {}", speechClient, audioContent);
             // Transcribes your audio file using the specified configuration.
             RecognitionConfig config = RecognitionConfig.newBuilder()
                     .setEncoding(RecognitionConfig.AudioEncoding.LINEAR16)
@@ -32,6 +35,8 @@ public class SpeechToTextRecognizer {
             RecognizeResponse response = speechClient.recognize(config, audio);
             List<SpeechRecognitionResult> results = response.getResultsList();
 
+            LOGGER.info("Finished transcribing audio");
+
             StringBuilder transcription = new StringBuilder();
             for (SpeechRecognitionResult result: results) {
                 transcription.append(result.getAlternatives(0).getTranscript());
@@ -39,6 +44,7 @@ public class SpeechToTextRecognizer {
 
             return transcription.toString();
         } catch (IOException e) {
+            LOGGER.error("Error when transcribing audio");
             throw new RuntimeException(e);
         }
     }
